6754c8a031556da589704cb3716f422ea0fef38d
z80code <pvl_p@tut.by>
2017-02-08 17:05:19 +0300

    update 7

---
+++ src/main/webapp/js/cinema.js
@@ -0,0 +1,162 @@
+var films = [
+    {
+        "filmId": 1,
+        "title": "Great Wall",
+        "year": 2017,
+        "director": {
+            "name": "director1"
+        },
+        "actors": [
+            {
+                "name": "actor1"
+            },
+            {
+                "name": "actor2"
+            },
+            {
+                "name": "actor3"
+            },
+            {
+                "name": "actor4"
+
+            }
+        ],
+
+        "description": "Great Wall erected to protect the Middle Kingdom from any threats, but with the enemy did not face even the most daring of its defenders. If you stop the invasion - the world will be destroyed. ",
+        "image": "",
+        "genres": [
+            {
+                "name": "fantasy"
+            }
+        ]
+    },
+    {
+        "filmId": 1,
+        "title": "Attraction",
+        "year": 2017,
+        "director": {
+            "name": "director1"
+        },
+        "actors": [
+            {
+                "name": "actor1"
+            },
+            {
+                "name": "actor2"
+            },
+            {
+                "name": "actor3"
+            },
+            {
+                "name": "actor4"
+
+            }
+        ],
+
+        "description": "Great Wall erected to protect the Middle Kingdom from any threats, but with the enemy did not face even the most daring of its defenders. If you stop the invasion - the world will be destroyed. ",
+        "image": "",
+        "genres": [
+            {
+                "name": "fantasy"
+            }
+        ]
+    },
+    {
+    "filmId": 1,
+        "title": "John Wick 2",
+        "year": 2017,
+        "director": {
+        "name": "director1"
+    },
+    "actors": [
+        {
+            "name": "actor1"
+        },
+        {
+            "name": "actor2"
+        },
+        {
+            "name": "actor3"
+        },
+        {
+            "name": "actor4"
+
+        }
+    ],
+
+        "description": "Great Wall erected to protect the Middle Kingdom from any threats, but with the enemy did not face even the most daring of its defenders. If you stop the invasion - the world will be destroyed. ",
+        "image": "",
+        "genres": [
+        {
+            "name": "fantasy"
+        }
+    ]
+},
+    {
+        "filmId": 1,
+        "title": "The Lego Movie: Batman ",
+        "year": 2017,
+        "director": {
+            "name": "director1"
+        },
+        "actors": [
+            {
+                "name": "actor1"
+            },
+            {
+                "name": "actor2"
+            },
+            {
+                "name": "actor3"
+            },
+            {
+                "name": "actor4"
+
+            }
+        ],
+
+        "description": "Great Wall erected to protect the Middle Kingdom from any threats, but with the enemy did not face even the most daring of its defenders. If you stop the invasion - the world will be destroyed. ",
+        "image": "",
+        "genres": [
+            {
+                "name": "fantasy"
+            }
+        ]
+    },
+    {
+        "filmId": 1,
+        "title": "",
+        "year": 2017,
+        "director": {
+            "name": "director1"
+        },
+        "actors": [
+            {
+                "name": "actor1"
+            },
+            {
+                "name": "actor2"
+            },
+            {
+                "name": "actor3"
+            },
+            {
+                "name": "actor4"
+
+            }
+        ],
+
+        "description": "Great Wall erected to protect the Middle Kingdom from any threats, but with the enemy did not face even the most daring of its defenders. If you stop the invasion - the world will be destroyed. ",
+        "image": "",
+        "genres": [
+            {
+                "name": "fantasy"
+            }
+        ]
+    }
+];
+
+function createFilmView(film) {
+
+
+}
